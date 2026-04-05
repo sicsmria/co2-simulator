294241dded9d4ac177d92ff760212d436deca8a3
sicsmria <jeongoogl@gmail.com>
2026-04-05 22:52:45 +0900

    Update subway_simul.py

--- subway_simul.py
+++ subway_simul.py
@@ -199,7 +199,7 @@ if st.sidebar.button("Clear All"): st.session_state.equipment_map = {}
 # =========================================================
 
 st.caption("@sicsmria")  
-st.title("Ultimate Underground Simulator (Peacetime vs Wartime)")
+st.title("Underground Simulator")
 
 elapsed_t = st.select_slider("🕒 Time Machine (Hours Elapsed)", options=np.round(np.linspace(0, 24, 241), 1), value=2.0)
 
